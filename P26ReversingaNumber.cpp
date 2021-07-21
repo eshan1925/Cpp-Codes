9f9dffded58005c88c6d112351af0702fd27a9c6
eshan1925 <78701779+eshan1925@users.noreply.github.com>
2021-07-21 11:34:08 +0530

    Add files via upload

---
+++ P26ReversingaNumber.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int dup;
+    cin>>n;
+    dup=n;
+    int reverse=0;
+    while (n>0) {
+    int lastdig=n%10;
+    reverse= reverse*10 + lastdig;
+    n=n/10;
+    }
+    cout<<reverse<<endl;
+    if(reverse==dup){
+        cout<<"palindrome";
+
+    }
+    else{
+        cout<<"Not palindrome";
+    }
+
+}
---
+++ P27Armstrong.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int dup;
+    int n;
+    cin>>n;
+    dup=n;
+    int lastdig;
+    int cub=0;
+    while(n>0){
+    lastdig=n%10;
+    cub=cub+(lastdig*lastdig*lastdig);
+    n=n/10;
+    }
+    cout<<cub<<endl;
+    if(cub==dup){
+        cout<<"Armstrong";
+    
+    }
+    else{
+        cout<<"Not Armstrong";
+    }
+}
