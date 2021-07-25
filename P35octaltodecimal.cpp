c6d6fcb7c548078364cc04fa2bb789db4b14a184
eshan1925 <78701779+eshan1925@users.noreply.github.com>
2021-07-25 12:27:25 +0530

    Add files via upload

---
+++ P35octaltodecimal.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int octaltodeci(int n){
+    int ans=0;
+    int x=1;
+    while(n>0){
+        int y=n%10;
+        ans=ans+x*y;
+        x=x*8;
+        n=n/10;
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<octaltodeci(n)<<endl;
+    
+
+}
